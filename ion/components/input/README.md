039c32ab03c4eceefd4026877c30d38add5c6ac9
Deeved Hiuston <deeved@gmail.com>
2023-09-26 14:11:53 -0300

    feat: input

---
+++ ion/components/input/README.md
@@ -0,0 +1,3 @@
+# @brisanet/ion/components/input
+
+Secondary entry point of `@brisanet/ion`. It can be used by importing from `@brisanet/ion/components/input`.
---
+++ ion/components/input/input.component.spec.ts
@@ -0,0 +1,199 @@
+import { SafeAny } from '@brisanet/ion/utils';
+import { render, screen, fireEvent } from '@testing-library/angular';
+import { IonInputComponent } from './input.component';
+import { IonInputProps, InputType } from './types';
+import userEvent from '@testing-library/user-event';
+
+const sut = async (customProps?: IonInputProps): Promise<void> => {
+  await render(IonInputComponent, {
+    componentProperties: customProps,
+  });
+};
+
+const user = userEvent.setup();
+
+describe('IonInputComponent', () => {
+  it('should render input with an empty placeholder if none is passed', async () => {
+    await sut();
+    const input = screen.getByTestId('input-element');
+    expect(input).not.toHaveAttribute('placeholder');
+  });
+  it('should render input with a given placeholder', async () => {
+    const placeholder = 'Search';
+    await sut({ placeholder });
+    const input = screen.getByTestId('input-element');
+    expect(input).toHaveAttribute('placeholder', placeholder);
+  });
+  it('should allow letters to be inputted', async () => {
+    await sut();
+    const inputValue = 'input';
+    fireEvent.change(screen.getByTestId('input-element'), {
+      target: { value: inputValue },
+    });
+    expect(screen.getByTestId('input-element')).toHaveValue(inputValue);
+  });
+
+  it.each(['text', 'password'] as InputType[])(
+    'should render type %s on input component',
+    async (type: InputType) => {
+      await sut({ inputType: type });
+      expect(screen.getByTestId('input-element')).toHaveAttribute('type', type);
+    }
+  );
+
+  it('should render input component disabled', async () => {
+    await sut({ disabled: true });
+    const element = screen.getByTestId('input-element');
+    expect(element).toBeDisabled();
+  });
+
+  it('should render input icon left', async () => {
+    const icon = 'trash';
+    await sut({ iconDirection: 'left', iconInput: icon });
+    expect(document.getElementById('ion-icon-' + icon)).toBeTruthy();
+  });
+
+  it('should render input icon right', async () => {
+    const icon = 'pencil';
+    await sut({ iconDirection: 'right', iconInput: icon });
+    expect(document.getElementById('ion-icon-' + icon)).toBeTruthy();
+  });
+
+  it('should render button when informed', async () => {
+    await sut({ inputButton: true });
+    const button = screen.getByTestId('input-button');
+    fireEvent.click(button);
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should render input icon button when informed', async () => {
+    await sut({ inputIconButton: true });
+    const button = screen.getByTestId('inputIcon-button');
+    fireEvent.click(button);
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should emit an event when clicked input button', async () => {
+    const clickEvent = jest.fn();
+    await sut({
+      inputButton: true,
+      clickButton: {
+        emit: clickEvent,
+      } as SafeAny,
+    });
+    fireEvent.click(screen.getByTestId('input-button'));
+    expect(clickEvent).toHaveBeenCalled();
+  });
+
+  it('should emit an event when clicked input icon button', async () => {
+    const clickEvent = jest.fn();
+    await sut({
+      inputIconButton: true,
+      clickButton: {
+        emit: clickEvent,
+      } as SafeAny,
+    });
+    fireEvent.click(screen.getByTestId('inputIcon-button'));
+    expect(clickEvent).toHaveBeenCalled();
+  });
+
+  it.each(['4', 4])(
+    'should render input component with text "valu" when the typed "values" and maxLength = 4',
+    async (maxLength) => {
+      const exampleText = 'values';
+      await sut({ maxLength });
+      const element: HTMLInputElement = screen.getByTestId('input-element');
+      await user.type(element, exampleText);
+      expect(element.value).toBe(exampleText.substring(0, Number(maxLength)));
+    }
+  );
+
+  it("should render input component without maxLength when don't sent this prop", async () => {
+    const text = 'text for input element test';
+    await sut({});
+    const element: HTMLInputElement = screen.getByTestId('input-element');
+    await user.type(element, text);
+    expect(element.value).toBe(text);
+  });
+
+  it.skip('should render input icon valid', async () => {
+    await sut();
+    expect(document.getElementById('icon-valid')).toBeTruthy();
+  });
+
+  it.skip('should render input icon invalid', async () => {
+    await sut();
+    expect(document.getElementById('icon-invalid')).toBeTruthy();
+  });
+
+  describe('valueChange', () => {
+    const mockFn = jest.fn();
+    const value = 'input';
+
+    beforeEach(async () => {
+      await sut({ valueChange: { emit: mockFn } as SafeAny });
+    });
+
+    afterEach(async () => {
+      mockFn.mockClear();
+    });
+
+    it('should change value when something is typed on input', async () => {
+      await user.type(screen.getByTestId('input-element'), value);
+      expect(screen.getByTestId('input-element')).toHaveValue(value);
+    });
+
+    it('should emit valueChange everytime a key is typed on input', async () => {
+      await user.type(screen.getByTestId('input-element'), value);
+      expect(mockFn).toHaveBeenCalledTimes(value.length);
+    });
+
+    it('should emit the value on the last emit', async () => {
+      await user.type(screen.getByTestId('input-element'), value);
+      expect(mockFn).toHaveBeenLastCalledWith(value);
+    });
+  });
+
+  describe('Clear Button events', () => {
+    const mockFn = jest.fn();
+    const value = 'input-with-clear-button';
+    let input: HTMLElement;
+
+    beforeEach(async () => {
+      await sut({
+        valueChange: { emit: mockFn } as SafeAny,
+        clearButton: true,
+      });
+      input = screen.getByTestId('input-element');
+    });
+
+    afterEach(async () => {
+      mockFn.mockClear();
+    });
+
+    it('should render the clear button when informed and input have value', async () => {
+      await user.type(input, value);
+      fireEvent.blur(input);
+      const clearButton = screen.getByTestId('clear-button');
+      expect(clearButton).toBeInTheDocument();
+    });
+
+    it('should change value to empty when clear button press', async () => {
+      await user.type(input, value);
+      fireEvent.click(screen.getByTestId('clear-button'));
+      expect(input).toHaveValue('');
+    });
+
+    it('should emit valueChange when clear button press', async () => {
+      await user.type(input, value);
+      fireEvent.click(screen.getByTestId('clear-button'));
+      expect(mockFn).toHaveBeenCalled();
+    });
+
+    it('should emit empty value when clear button press', async () => {
+      await user.type(input, value);
+      fireEvent.click(screen.getByTestId('clear-button'));
+      expect(mockFn).toHaveBeenLastCalledWith('');
+    });
+  });
+});
